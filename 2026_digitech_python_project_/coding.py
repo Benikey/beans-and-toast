f5d86d7ec9c942fe4c9f0eda7685889e9a113a26
Benikey <joanna.jiang0712@gmail.com>
2026-02-05 12:35:43 +1300

    Create coding.py

---
+++ 2026_digitech_python_project_/coding.py
@@ -0,0 +1,17 @@
+
+
+print (123)
+# yo im not sure if i did this right like not the code the file thing
+
+print ("hello world")
+
+# data types -->
+# intergers and decimals (float) strings("text") and booleans (true or false)
+
+# variables!!!!!!!!!! (must be lowercase no spaces) 
+name = "something something"
+print (name)
+# F STRINGS
+print (f"my name is {name}")
+user = input("what is your name?")
+print ("your name is {user}")
